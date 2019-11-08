afa7eec2edd65c3a74e75914b4bbd44b36877502
Ant-Quanique Dancy <antdancy95@gmail.com>
2019-11-08 10:02:48 -0500

    added an authentication fuction

--- ecommerce-client/src/Helpers/helpers.js
+++ ecommerce-client/src/Helpers/helpers.js
@@ -1,6 +1,35 @@
+import axios from "axios";
+
 export const authenticate = (data, next) => {
   if (typeof window !== "undefined") {
       localStorage.setItem("User", JSON.stringify(data));
       next();
   }
 };
+
+export const signout = () => {
+  if (typeof window !== "undefined") {
+    localStorage.removeItem("User")
+  };
+    return axios.delete("http://localhost:3001/api/logout",{ withCredentials: true })
+    .then((response) => {
+      console.log(response)
+    })
+    .catch(err => {
+      console.log("Logout ERROR", err)
+    });
+  
+};
+  
+export const isAuthenticated = () => {
+  if (typeof window === "undefined") {
+    return false
+  };
+
+  if (localStorage.getItem("User")) {
+    return JSON.parse(localStorage.getItem("User"))
+  } else {
+    return false;
+  }
+}
+
