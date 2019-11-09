375f310374912d28b054b964d7b165700458e024
Ant-Quanique Dancy <antdancy95@gmail.com>
2019-11-09 15:47:10 -0500

    added env variables

---
+++ ecommerce-client/src/Helpers/privateRoutes.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Route, Redirect } from 'react-router-dom';
+import { isAuthenticated } from './helpers'
+
+
+const PrivateRoute = ({ component: Component, ...rest }) => (
+  <Route {...rest} render={props => isAuthenticated() ?
+    (
+      <Component {...props} />
+    ) : (
+      <Redirect to={{ pathname: '/signin', state: { from: props.location } }} />
+    )} />
+);
+
+export default PrivateRoute;
