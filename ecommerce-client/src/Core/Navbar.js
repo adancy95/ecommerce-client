fc93da14917847e8609fbb9bce90b65cbb852095
Ant-Quanique Dancy <antdancy95@gmail.com>
2019-11-09 15:46:13 -0500

    removed one Navbar

--- ecommerce-client/src/Core/Navbar.js
+++ ecommerce-client/src/Core/Navbar.js
@@ -2,7 +2,6 @@ import React, {Fragment} from 'react';
 import { Link, withRouter } from 'react-router-dom'
 import { MdAccountCircle } from 'react-icons/md';
 import { MdShoppingBasket } from 'react-icons/md';
-import { MdSearch } from 'react-icons/md';
 import './Core.css';
 import { signout, isAuthenticated } from '../Helpers/helpers'
 
@@ -30,40 +29,13 @@ class Navbar extends React.Component{
                   <li><Link to="/" onClick={() => signout()}>Signout</Link></li>
                 )} 
               </ul>
-            </div>
-          </nav>
-        </div>
-        <div className="navbar-fixed nav-extended">
-          <nav>
-            
-            <div className="nav-wrapper white">
-              <ul className="left hide-on-med-and-down">
-                <li><Link to="/"> <span className="black-text">Mina Cre8s</span></Link></li> 
-              </ul>
-              <form className="nav-content brand-logo center">
-                <div className="input-field">
-                  <input id="search" type="search" required/>
-                  <label className="label-icon" htmlFor="search"><MdSearch className="label-icons large black-text" /></label>
-                </div>
-              </form>
               <ul className="right hide-on-med-and-down">
-                <li><Link to="/"><MdShoppingBasket className="material-icons black-text"/></Link></li>
-                <li><Link to="/"><MdAccountCircle className="material-icons black-text" /></Link></li>
-              </ul>
-            </div>
-            <hr className="grey lighten-5"/>
-            <div className="nav-content brand-logo center">
-              <ul className="tabs tabs-transparent ">
-                <li className="tab"><Link to="product/bows"><span className="black-text">Bows</span></Link></li>
-                <li className="tab"><Link to="product/onesies"><span className="black-text">Onesies</span></Link></li>
-                <li className="tab"><Link to="product/tshirts"><span className="black-text">T-Shirts</span></Link></li>
-                <li className="tab"><Link to="product/tutus"><span className="black-text">Tutus</span></Link></li>
-                
+                <li><Link to="/"><MdShoppingBasket className="material-icons"/></Link></li>
+                <li><Link to="/user/dashboard"><MdAccountCircle className="material-icons" /></Link></li>
               </ul>
             </div>
           </nav>
         </div>
-        
 
       </div>
       
