212a67effedd127452a4b7bf1c77d202899a3d25
Ant-Quanique Dancy <antdancy95@gmail.com>
2019-11-10 10:16:11 -0500

    moved content in a container

--- ecommerce-client/src/User/UserDashboard.js
+++ ecommerce-client/src/User/UserDashboard.js
@@ -1,54 +1,80 @@
 import React from 'react';
 import { isAuthenticated } from '../Helpers/helpers';
-import SideNav from './SideNav';
 import "../Core/Core.css"
+import { Link} from 'react-router-dom'
 
-class Dashboard extends React.Component{
-  render() {
+class UserDashboard extends React.Component{
+
+  userContent = () => {
     const { data : {userDoc : {firstName, lastName, email, role, _id}} } = isAuthenticated()
     return (
-      <div>
-        <SideNav />
-        <div className="main">
-        <div  id="content" className="row">
-          <div className="col s12 m6">
             <div className="card blue-grey darken-1">
               <div className="card-content">
                 <span className="card-title white-text">User Information</span>
-                <ul class="collection">
-                  <li class="collection-item">{firstName} {lastName}</li>
-                  <li class="collection-item">{email}</li>
-                  <li class="collection-item">Role: {role === 1 ? 'Admin' : 'User'}</li>
-                  <li class="collection-item">id: {_id}</li>
-                </ul> 
-              </div>
-              <div className="card-action">
-                <a href="#">Edit User</a>
-              </div>
-            </div>
-          </div>
-        </div>
-
-        <div className="row">
-          <div className="col s12 m6">
-            <div className="card blue-grey darken-1">
-              <div className="card-content">
-                <span className="card-title white-text">Order History</span>
-                <ul class="collection">
-                  <li class="collection-item">Date</li>
-                  <li class="collection-item">Time</li>
-                  <li class="collection-item">Items Purchased</li>
-                  <li class="collection-item">Total</li>
+                <ul className="collection">
+                  <li className="collection-item">{firstName} {lastName}</li>
+                  <li className="collection-item">{email}</li>
+                  <li className="collection-item">Role: {role === 1 ? 'Admin' : 'User'}</li>
+                  <li className="collection-item">id: {_id}</li>
                 </ul> 
               </div>
             </div>
+    )
+  }
+
+  orderHistory = () => (
+  
+          <div className="card blue-grey darken-1">
+            <div className="card-content">
+              <span className="card-title white-text">Order History</span>
+              <ul className="collection">
+                <li className="collection-item">Date</li>
+                <li className="collection-item">Time</li>
+                <li className="collection-item">Items Purchased</li>
+                <li className="collection-item">Total</li>
+              </ul> 
+            </div>
           </div>
-        </div>
+   
+
+      
+  )
+
+  leftNav = () => 
+    (
+     
+      <ul id="slide-out" style={{marginTop:"65px"}} className="sidenav sidenav-fixed leftnav" >
+        <li><Link to="/user/dashboard">Dashboard</Link></li>
+        <li><Link to="/user/dashboard/orders">Order History</Link></li>
+        <li><Link to="/user/dashboard/profile">User Profile</Link></li>
+      
+      </ul>
+
+
+      
+    )
+  
+  render() {
+  
+    return (
+
+          <div className="row">
+
+            <div className="col s3">
+            {this.leftNav()}
+            </div>
+
+            <div className="col s9">
+              
+            {this.userContent()}
+            {this.orderHistory()}
+            </div>
 
         </div>
-      </div>
+     
+       
     )
   }
 }
 
-export default Dashboard
+export default UserDashboard
