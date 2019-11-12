7df55411f6a9a1daf4faedb96019b1c9cb331a43
Ant-Quanique Dancy <antdancy95@gmail.com>
2019-11-11 20:52:30 -0500

    created a route to restrict access to admin routes

---
+++ ecommerce-client/src/Helpers/AdminRoutes.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Route, Redirect } from 'react-router-dom';
+import { isAuthenticated } from './helpers'
+
+
+
+const AdminRoute = ({ component: Component, ...rest }) => (
+  <Route {...rest} render={props => (isAuthenticated() && isAuthenticated().data.userDoc.role === 1) ?
+    (
+      < Component {...props} />
+    ) : (
+      <Redirect to={{ pathname: '/signin', state: { from: props.location } }} />
+    )} />
+);
+
+export default AdminRoute;
