e4c4999cfe294d46c4e7cb8927d7c116f83abd75
Ant-Quanique Dancy <antdancy95@gmail.com>
2019-11-11 20:48:14 -0500

    Added two new routes. Admin dashboard and add category

--- ecommerce-client/src/App.js
+++ ecommerce-client/src/App.js
@@ -6,7 +6,9 @@ import Home from './Core/Home';
 import Navbar from './Core/Navbar';
 import UserDashboard from './User/UserDashboard';
 import AdminDashboard from './User/AdminDashboard';
-import PrivateRoute from './Helpers/privateRoutes'
+import PrivateRoute from './Helpers/privateRoutes';
+import AdminRoute from './Helpers/privateRoutes'
+import AddCategory from './Admin/Category/AddCategory';
 
 class App extends React.Component {
   constructor(props) {
@@ -26,8 +28,9 @@ class App extends React.Component {
             <Route path="/" exact component={Home}/>
             <Route path="/signin" exact component={Signin} />
             <Route path="/signup" exact component={Signup} />
-              <PrivateRoute path="/user/dashboard" exact component={UserDashboard} />
-              <PrivateRoute path="/admin/dashboard" exact component={AdminDashboard} />
+            <PrivateRoute path="/user/dashboard" exact component={UserDashboard} />
+            <AdminRoute path="/admin/dashboard" exact component={AdminDashboard} />
+            <AdminRoute path="/admin/dashboard/categories/create" exact component={AddCategory} />
           </Switch>
         </div>
         
