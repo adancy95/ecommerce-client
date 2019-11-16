a6f8dc911844e61731f572602ca56c91e72305ee
Ant-Quanique Dancy <antdancy95@gmail.com>
2019-11-16 18:09:32 -0500

    added a edit and delete button

--- ecommerce-client/src/Admin/Product/Products.js
+++ ecommerce-client/src/Admin/Product/Products.js
@@ -1,7 +1,11 @@
 import React, { Component } from 'react';
 import "../../Core/Core.css"
 import axios from 'axios';
-import {AdminleftNav} from '../../User/AdminLeftNav'
+import { AdminleftNav } from '../../User/AdminLeftNav';
+import { MdModeEdit  } from 'react-icons/md';
+import { MdDeleteForever } from 'react-icons/md';
+import { Link } from 'react-router-dom';
+import update from 'immutability-helper';
 
 class Products extends Component {
 
@@ -10,6 +14,7 @@ class Products extends Component {
         this.state = {
           products: []
         }
+      this.removeItem = this.removeItem.bind(this)
     }
 
     componentDidMount() {
@@ -19,27 +24,49 @@ class Products extends Component {
           console.log(this.state.products)
         })
     }
+    removeItem(index, id) {
+      axios.delete(`${process.env.REACT_APP_API_URL}/products/delete/${id}`,{ withCredentials: true })
+        .then((response) => {
+        this.setState((prevState) => ({
+          products: update(prevState.products, {$splice: [[index, 1]]})
+        }))
+      })
+      .catch(err => {
+        console.log("delete error", err)
+      });
+        
+      }
+    
   
   showProducts() {
     return (
       <div>
-        <h2>products</h2>
+        <h2>Products</h2>
         <table>
           <thead>
             <tr>
               <th>Name</th>
               <th>Price</th>
               <th>Quantity</th>
+              <th>Edit Category</th>
+              <th>Delete Category</th>
             </tr>
           </thead>
           <tbody>
-            {this.state.products.map(product =>
+            {this.state.products.map((product, index) =>
               <tr key={product._id}>
                 <td>
                 {product.name.charAt(0).toUpperCase() + product.name.slice(1)}
                 </td>
                 <td>${product.offers.regularPrice}</td>
                 <td>{product.availability.quantity}</td>
+                <td>
+                  <Link to={`/admin/dashboard/products/edit/${product._id}`}><MdModeEdit className="material-icons" /></Link>
+                 
+                </td>
+                <td>
+                  <Link to="/admin/dashboard/products" onClick={() => this.removeItem(index, product._id )}><MdDeleteForever className="material-icons" /></Link>
+                </td>
             </tr>
             )}
           </tbody>
