116fc7b99058507e6dbf1957c9eb8c8104bec81c
Ant-Quanique Dancy <antdancy95@gmail.com>
2019-12-01 12:13:14 -0500

    refactored product card and product page

--- ecommerce-client/src/Client/ProductCard.js
+++ ecommerce-client/src/Client/ProductCard.js
@@ -1,21 +1,31 @@
 import React from 'react';
+import { IoIosStar } from "react-icons/io";
 
 export default class ProductCard extends React.Component{
   render() {
     return (
-      <div className="row">
-        <div className="col s4 m3">
-          <div className="card">
-            <div className="card-image">
-              <img src="/images/tutu.jpg" alt="productImage"/>
+      <div className="product-card">
+        <div className="product-card-image">
+          <img src="https://i.pravatar.cc/200" alt="productImage" />
+        </div>
+        <div className="product-card-details">
+          <div className="product-card-title">
+            <p className="product-card-name primary-text">Product Name</p>
+            <p className="product-card-description secondary-text">Description goes here</p>
+          </div>
+          <div className="product-card-subdetails">
+            <p className="product-card-price textColor tertiary-text">$19.99</p>
+            <div className="product-card-rating tertiary-text">
+              <span><IoIosStar/></span>
+              <span><IoIosStar/></span>
+              <span><IoIosStar/></span>
+              <span><IoIosStar/></span>
+              <span><IoIosStar/></span>
             </div>
-            <div className="card-content">
-            <span className="card-title">Button Shirt</span>
-              <p>Description goes here. Mock Data</p>
-            </div>
-          </div> 
+          </div>
         </div>
       </div>
+      
     )
   }
 }
--- ecommerce-client/src/Client/ProductsComponent.js
+++ ecommerce-client/src/Client/ProductsComponent.js
@@ -4,10 +4,25 @@ import ProductCard from './ProductCard'
 export default class ProductsComponent extends React.Component{
   render() {
     return (
-      <div>
-        <ProductCard/>
-
-      </div>
+      <div className="product-listing">
+        <div className="product-filters"> Side Nav Filters
+          <ul>
+            <li>Categories</li>
+            <li>Filter By Price</li>
+            <li>Filter By Size</li>
+            <li>Filter By Color</li>
+          </ul>
+        </div>
+        <div className="product-items">
+          <ProductCard />
+          <ProductCard />
+          <ProductCard />
+          <ProductCard />
+          <ProductCard />
+          <ProductCard />
+          
+        </div>
+        </div>
     )
   }
 }
