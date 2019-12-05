fe1ea7786fd3ec2517576cda2f0a0a14fbdc6b26
Ant-Quanique Dancy <antdancy95@gmail.com>
2019-12-04 20:16:46 -0500

    changed the sizes to cirlce buttons

---
+++ ecommerce-client/src/Client/CircleButton.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const CircleButton = (props) => {
+  return (
+    <a href="#"><span className="circle-button">{props.text}</span></a> 
+   
+    )
+  
+}
+
+export default CircleButton
--- ecommerce-client/src/Client/SizeFilter.js
+++ ecommerce-client/src/Client/SizeFilter.js
@@ -1,16 +1,17 @@
 import React from 'react';
+import CircleButton from './CircleButton'
 
 export default class SizeFilter extends React.Component{
   render() {
     return (
       <div>
         <h6>Filter By Size</h6>
-        <ul>
-          <li className="filter-size">X-small</li>
-          <li className="filter-size">Small</li>
-          <li className="filter-size">Medium</li>
-          <li className="filter-size">Large</li>
-          <li className="filter-size">X-Large</li>
+        <ul className="sizes">
+          <li className="filter-size"><CircleButton text="XS" /></li>
+          <li className="filter-size"><CircleButton text="S" /></li>
+          <li className="filter-size"><CircleButton text="M" /></li>
+          <li className="filter-size"><CircleButton text="L"/></li>
+          <li className="filter-size"><CircleButton text="XL"/></li>
         </ul>
       </div>
     )
