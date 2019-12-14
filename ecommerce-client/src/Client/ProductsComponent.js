ea755a55f3d28436d0870616380ffaae9cbf1c29
Ant-Quanique Dancy <antdancy95@gmail.com>
2019-12-13 22:59:04 -0500

    used axios to get the products from the backend

--- ecommerce-client/src/Client/ProductsComponent.js
+++ ecommerce-client/src/Client/ProductsComponent.js
@@ -4,8 +4,25 @@ import SizeFilter from './SizeFilter'
 import ColorFilter from './ColorFilter'
 import CategoryFilter from './CategoryFilter'
 import PriceFilter from './PriceFilter'
+import axios from 'axios'
 
 export default class ProductsComponent extends React.Component{
+  constructor(props) {
+    super(props)
+    this.state = {
+      products: undefined
+    }
+  }
+
+  componentDidMount() {
+    axios.get(`${process.env.REACT_APP_API_URL}/products`)
+      .then(({ data: product }) => {
+        this.setState({
+          products : product.products
+        });
+        console.log(this.state)
+      });
+  }
   render() {
     return (
       <div className="product-listing">
