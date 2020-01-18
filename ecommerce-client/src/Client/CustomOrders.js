fc53e22671b1e090e0e2c668c9be68c86671ed21
Ant-Quanique Dancy <antdancy95@gmail.com>
2020-01-17 20:07:34 -0500

    added form items to the state

--- ecommerce-client/src/Client/CustomOrders.js
+++ ecommerce-client/src/Client/CustomOrders.js
@@ -1,6 +1,28 @@
 import React from 'react';
 
 export default class CustomOrders extends React.Component{
+  constructor(props) {
+    super(props)
+    this.state = {
+      firstName: "",
+      lastName: "",
+      emailAddress: "",
+      phone: "",
+      mailingAddressOne: "",
+      mailingAddressTwo: "",
+      city: "",
+      state: "",
+      country: "",
+      zipCode: 0,
+      sizeType: "",
+      sizes: "",
+      colors: "",
+      shirtFit: "",
+      designImage: "",
+      designText: "",
+      quantity: 0
+    }
+  }
   render() {
     return (
       <div>
