b0c6caf259777460f5a9ca9747514762f8e83350
Ant-Quanique Dancy <antdancy95@gmail.com>
2020-01-21 19:32:09 -0500

    added design fields to the custom order form

--- ecommerce-client/src/Client/CustomOrders.js
+++ ecommerce-client/src/Client/CustomOrders.js
@@ -40,7 +40,42 @@ export default class CustomOrders extends React.Component{
               <input  id="phone" type="text" name="phone" className="validate" value={this.state.phone} onChange={this.handleChange}/>
               <label className="active" htmlFor="phone">Phone Number</label>
             </div>
+            <div className="input-field col s3">
+              <input  id="sizeType" type="text" name="sizeType" className="validate" value={this.state.sizeType} onChange={this.handleChange}/>
+              <label className="active" htmlFor="sizeType">Size Type</label>
+            </div>
+            <div className="input-field col s3">
+              <input  id="sizes" type="text" name="sizes" className="validate" value={this.state.sizes} onChange={this.handleChange}/>
+              <label className="active" htmlFor="sizes">Sizes</label>
+            </div>
+            <div className="input-field col s3">
+              <input  id="colors" type="text" name="colors" className="validate" value={this.state.colors} onChange={this.handleChange}/>
+              <label className="active" htmlFor="colors">Color</label>
+            </div>
+            <div className="input-field col s3">
+              <input  id="shirtFit" type="text" name="shirtFit" className="validate" value={this.state.shirtFit} onChange={this.handleChange}/>
+              <label className="active" htmlFor="shirtFit">Shirt Fit</label>
+            </div>
+            <div className="input-field col s12">
+              <input  id="quantity" type="number" name="quantity" className="validate" value={this.state.quantity} onChange={this.handleChange}/>
+              <label className="active" htmlFor="quantity">Quantity</label>
+            </div>
+            <h6> Tell us about your custom design</h6>
+            <div className="input-field col s12">
+              <input  id="designText" type="text" name="designText" className="validate" value={this.state.designText} onChange={this.handleChange}/>
+              <label className="active" htmlFor="designText">Design Text</label>
+            </div>
+            <div className="file-field input-field col s12">
+           <div className="btn-small waves-effect waves-light btnColor">
+              <span>Upload Design</span>
+              <input type="file" accept="image/*"  onChange={this.handleImage}/>
+            </div>
+            <div className="file-path-wrapper">
+              <input className="file-path validate" type="text"/>
+            </div>
           </div>
+          </div>
+         
 
         </form>
       </div>
